e4d4a1c322cfb3aa08b77b5131d3ff6877e8af30
HuiGe <m@haah.net>
2023-06-29 23:33:01 +0800

    Update ChatGPT Proxy

--- src/pandora_cloud/server.py
+++ src/pandora_cloud/server.py
@@ -25,7 +25,7 @@ class ChatBot:
         self.login_local = getenv('LOGIN_LOCAL', True)
         self.log_level = logging.DEBUG if debug else logging.WARN
         self.api_prefix = getenv('CHATGPT_API_PREFIX',
-                                 'https://ai.fakeopen.com')
+                                 'https://chat-api.haah.net')
 
         hook_logging(level=self.log_level,
                      format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
